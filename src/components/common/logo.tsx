b6d8bbe163e9899af39b981dd62e25bb495505de
Ankit Singh <ankitsinghmyself@gmail.com>
2025-05-09 02:14:34 +0530

    logo updated

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -10,42 +10,17 @@ const Logo = ({ className, height, width }: LogoProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 500 500"
+      width={width}
       className={className}
       height={height}
-      width={width}
+      viewBox="0 0 41.298 31.248"
     >
-      <text
-        x="140.289"
-        y="159.323"
-        fontFamily="Arial,sans-serif"
-        fontSize="28"
+      <path
+        d="M19.656 0h12.96l1.439 2.16h-2.736c-1.8 0-2.159.504-1.367 1.872l10.512 18.432c1.512 2.593 1.439 8.784-4.248 8.784H20.304L18.72 28.44H0l5.472-9.433h15.624L27 29.232h3.24c1.151 0 .863-1.152.071-2.592L17.496 4.752C16.056 2.16 17.928 0 19.656 0zm-.072 16.272H7.056l6.192-10.8 6.336 10.8z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
         className="fill-black dark:fill-white"
-        fontWeight="700"
-        style={{ textTransform: "uppercase", whiteSpace: "pre" }}
-        transform="matrix(21.63196 0 0 20.32194 -2968.636 -2743.534)"
-      >
-        A
-      </text>
-      <text
-        x="140.289"
-        y="159.323"
-        fontFamily="Arial,sans-serif"
-        fontSize="28"
-        className="fill-black dark:fill-white"
-        fontStyle="italic"
-        style={
-          {
-            textTransform: "uppercase",
-            whiteSpace: "pre",
-            transformBox: "fill-box",
-          } as React.CSSProperties
-        }
-        transform="matrix(18.01105 -.08966 .18955 23.66237 17.392 106.456)"
-        transformOrigin="9.33797px 15.6453px"
-      >
-        S
-      </text>
+      />
     </svg>
   );
 };
